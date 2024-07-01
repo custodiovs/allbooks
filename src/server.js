3ed2602e80595e36143f2065bab0cea66e654a12
custodiovs <vctrsc99@outlook.com>
2024-07-01 20:13:55 -0300

    Altera rota

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do Allbooks!")
-  console.log("API disponível através da url http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> adf73f64697982f5d2e5d4356809a4440af56891
 })
